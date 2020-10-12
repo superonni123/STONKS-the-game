f9a60458415db6181db619a345231bce2e11cc94
onni <lpertti71@gmail.com>
2020-10-12 20:26:35 +0300

    investment update

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import random
 import time
 
 money = 0
+owned_stock1 = 0
+stock1price = random.randrange(1, 100, 1)
 
 print("initiating loading")
 time.sleep(1)
@@ -27,12 +29,43 @@ while True:
         print("Help     show this page")
         print("Exit     exits the game")
         print("Work     you gain money from working")
-        print("Money    shows how many money you got")
+        print("Check    shows how many money and stocks you got")
+        print("Invest   you can buy stocks and make money")
 
-    elif cmd == "money":
+    elif cmd == "check":
         print("you have " + str(money) + " money")
+        print("you have " + str(owned_stock1) + " stock1")
 
     elif cmd == "work":
         money_add = random.randrange(5, 20, 1)
         print("you gained " + str(money_add) + " money from work")
         money = money + money_add
+
+    elif cmd == "invest":
+        while True:
+            cmd2 = input("action: ")
+            if cmd2 == 'exit':
+                break
+
+            elif cmd2 == 'check':
+                a = random.randrange(0, 10000000, 1)
+                if a < 5:
+                    stock1price = stock1price + random.randrange(1, 100, 1)
+
+                elif a > 5:
+                    stock1price = stock1price - random.randrange(1, 100, 1)
+
+                print("stock 1 costs " + str(stock1price) + " money")
+                print("")
+
+            elif cmd2 == 'buy':
+                buy = int(input("how many stocks to buy: "))
+                owned_stock1 = owned_stock1 + buy
+                money = money - (stock1price * buy)
+                print("successfully bought " + str(buy) + " stocks")
+
+            elif cmd2 == 'sell':
+                sell = int(input("how many stock to sell: "))
+                owned_stock1 = owned_stock1 - sell
+                money = money + (stock1price * sell)
+                print("successfully sold " + str(sell) + " stocks")
